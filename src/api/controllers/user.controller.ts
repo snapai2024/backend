e936a096494db915b40fd8f37a01a86f0d939644
ludovic.sobrero <sobrero.ludovic@gmail.com>
2024-01-22 13:38:58 +0100

    feat: GET /me/collections

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -9,6 +9,7 @@ export interface UserController {
   getAll(req: CustomRequest, res: Response): any;
   getById(req: CustomRequest, res: Response): any;
   getCurrent(req: CustomRequest, res: Response): any;
+  getMyCollections(req: CustomRequest, res: Response): any;
   register(req: CustomRequest, res: Response): any;
   update(req: CustomRequest, res: Response): any;
   delete(req: CustomRequest, res: Response): any;
@@ -39,9 +40,15 @@ class _UserController implements UserController {
 
   public async getCurrent(req: CustomRequest, res: Response) {
     try {
-      const user: User = await UserService.getById(Number(req.user!.id));
+      res.status(200).json(req.user);
+    } catch (err) {
+      res.status(400).json({ error: err as CausalError });
+    }
+  }
 
-      res.status(200).json(user);
+  public async getMyCollections(req: CustomRequest, res: Response) {
+    try {
+      res.status(200).json(req.user?.collections);
     } catch (err) {
       res.status(400).json({ error: err as CausalError });
     }
--- src/api/routes/user.router.ts
+++ src/api/routes/user.router.ts
@@ -7,6 +7,7 @@ const userRouter: Router = Router();
 
 userRouter.get('/', AuthenticationMiddleware.checkPermissions([RoleNameEnum.ADMIN]), UserController.getAll);
 userRouter.get('/me', AuthenticationMiddleware.checkPermissions([]), UserController.getCurrent);
+userRouter.get('/me/collections', AuthenticationMiddleware.checkPermissions([]), UserController.getMyCollections);
 userRouter.get('/:id', UserController.getById);
 userRouter.post('/', UserController.register);
 userRouter.patch('/:id', AuthenticationMiddleware.checkPermissions([]), UserController.update);
